afb41a39df05a654d1a326499f4841bd627a5327
cailu <cailudys@gmail.com>
2022-08-07 20:29:39 +0800

    使用自己实现的tinyreact中的createElement方法，创建了一个虚拟dom

--- src/TinyReact/createElement.js
+++ src/TinyReact/createElement.js
@@ -1,5 +1,10 @@
 // 用于创建虚拟DOM
 // ...children的写法，表示我们要把后面的参数都放到children这个数组当中
 export default function createElement(type, props, ...children) {
-  //
+  // 返回值是虚拟DOM,实际就是一个JavaScript对象。
+  return {
+    type,
+    props,
+    children,
+  };
 }
